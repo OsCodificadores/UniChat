51fde0137ef5b8070985774856c6ee6388fb0960
Kaleo <kaleobonatto@gmail.com>
2022-09-24 13:00:19 -0300

    removed not useds

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import {auth, provider} from "../../services/firebase"
-import * as C from "./styles";
 import Login from "../../components/Login";
 
 const LoginPage = () =>{
